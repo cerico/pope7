6354664c6980e837821557b949c9d5bcec203027
cerico <garethrobertlee@gmail.com>
2022-05-09 17:11:10 +0100

    init

---
+++ post.js
@@ -0,0 +1,38 @@
+const fs = require("fs")
+const now = new Date().toDateString();
+const iso = new Date().toISOString()
+const displayDate = (new Date()).toLocaleDateString("en-GB",{day: 'numeric', month:'short'});
+const md = `---
+title: Hello world
+author: ''
+publishDate: ${now}
+displayDate: ${displayDate}
+cover: https://i.ibb.co/swkd4P2/ccc.jpg
+setup: |
+  import Layout from '../../layouts/BlogPost.astro'
+  import Cool from '../../components/Author.astro'
+description: hello world example
+tags:
+  - bluey
+  - yellow
+---
+
+## Hello world
+
+Here is a starter post with an example codeblock.
+
+
+\`\`\`js
+const makeRequest = (verb, endPoint, data) => {
+  return fetch(endPoint, {
+    headers: headers(),
+    credentials: 'include',
+    method: verb,
+    body: JSON.stringify(data)
+  })
+}
+\`\`\`
+`
+
+let output = md
+fs.writeFileSync(`./src/pages/posts/post-${iso}.md`, output)
